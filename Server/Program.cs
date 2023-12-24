7e6b4276810f0896a9756777b3dac6acfb8dd3d7
Andrey <aerem2910@gmail.com>
2023-12-24 14:01:38 +0300

    TCPIP

---
+++ Server/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+class Server
+{
+    static void Main()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 8888);
+        listener.Start();
+        Console.WriteLine("Сервер запущен...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Клиент подключен.");
+
+            Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+            clientThread.Start(client);
+        }
+    }
+
+    static void HandleClient(object obj)
+    {
+        TcpClient tcpClient = (TcpClient)obj;
+        NetworkStream stream = tcpClient.GetStream();
+
+        StreamReader reader = new StreamReader(stream);
+        StreamWriter writer = new StreamWriter(stream);
+
+        try
+        {
+            string data = reader.ReadLine();
+            Console.WriteLine($"Получены данные от клиента: {data}");
+
+            // Фильтрация четных чисел
+            string[] numbers = data.Split(',');
+            string result = string.Join(",", Array.FindAll(numbers, s => int.Parse(s) % 2 == 0));
+
+            Console.WriteLine($"Отфильтрованные четные числа: {result}");
+
+            // Отправка результата клиенту
+            writer.WriteLine(result);
+            writer.Flush();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+        finally
+        {
+            tcpClient.Close();
+        }
+    }
+}
---
+++ TCPCientServer/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+class Client
+{
+    static void Main()
+    {
+        Console.Write("Введите числа через запятую: ");
+        string numbers = Console.ReadLine();
+
+        using (TcpClient client = new TcpClient("127.0.0.1", 8888))
+        {
+            using (NetworkStream stream = client.GetStream())
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        // Отправка чисел серверу
+                        writer.WriteLine(numbers);
+                        writer.Flush();
+
+                        // Получение отфильтрованных четных чисел от сервера
+                        string result = reader.ReadLine();
+                        Console.WriteLine($"От сервера получены четные числа: {result}");
+                    }
+                }
+            }
+        }
+    }
+}
